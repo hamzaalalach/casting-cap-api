d155fc5cca59a02b5313d8ff29a04f7e61360db1
Hamza Alalach <hamzaalalach@gmail.com>
2020-05-18 20:16:50 +0100

    First commit

---
+++ src/models.py
@@ -0,0 +1,78 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.environ.get("DATABASE_URL") or "postgres://postgres:0000@localhost:5432/capstone"
+
+db = SQLAlchemy()
+
+def create_all():
+	db.create_all()
+
+def setup_db(app, database_path=database_path):
+	app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	db.app = app
+	db.init_app(app)
+
+class Movie(db.Model):
+	__tablename__ = 'movies'
+
+	id = Column(Integer, primary_key=True)
+	title = Column(String(120), nullable=False)
+	release_date = Column(String(120), nullable=False)
+
+	def __init__(self, title, release_date):
+		self.title = title
+		self.release_date = release_date
+
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def update(self):
+		db.session.commit()
+
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+
+	def format(self):
+		return {
+			'id': self.id,
+			'title': self.title,
+			'release_date': self.release_date
+		}
+
+class Actor(db.Model):
+	__tablename__ = 'actors'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(120), nullable=False)
+	age = Column(Integer, nullable=False)
+	gender = Column(String(120), nullable=False)
+
+	def __init__(self, name, age, gender):
+		self.name = name
+		self.age = age
+		self.gender = gender
+
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def update(self):
+		db.session.commit()
+
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'age': self.age,
+			'gender': self.gender
+		}
